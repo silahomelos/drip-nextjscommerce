7e88cd7addf8993c51a59fcfe4f33246fdc886bd
beowulf518 <beowulf93518@gmail.com>
2021-04-26 01:41:27 -0400

    add stacked card

--- components/ui/StackedCard/StackedCard.tsx
+++ components/ui/StackedCard/StackedCard.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
+import Draggable from 'react-draggable'
 import cn from 'classnames'
 import s from './StackedCard.module.scss'
 type Props = {
@@ -6,29 +7,61 @@ type Props = {
 }
 const StackedCard: React.FC<Props> = ({ index }) => {
   const textList = [
-    'DYNAMIC NFT MINI DESCRIPTION GOES HERE',
-    'HYBRID FASHION NFTs',
-    'UNLOCKABLE CONTENT',
+    { index: 1, text: 'DYNAMIC NFT MINI DESCRIPTION GOES HERE' },
+    { index: 2, text: 'HYBRID FASHION NFTs' },
+    { index: 3, text: 'UNLOCKABLE CONTENT' },
+    { index: 4, text: 'DYNAMIC NFT MINI DESCRIPTION Text' },
   ]
-  const [topText, setTopText] = useState(textList[0])
+  const [stackTexts, setStackTexts] = useState(textList)
   useEffect(() => {
-    console.log(index)
-    setTopText(textList[index])
+    const [movedItem] = stackTexts.filter((item) => item.index == index + 1)
+    setStackTexts([
+      ...stackTexts.filter((item) => item.index !== index + 1),
+      movedItem,
+    ])
   }, [index])
+
+  const handleStop = (index: number) => {
+    const [movedItem] = stackTexts.filter((item) => item.index == index)
+    setStackTexts([
+      movedItem,
+      ...stackTexts.filter((item) => item.index !== index),
+    ])
+  }
+
   return (
     <div className={s.cardContainer}>
-      <div className={cn(s.stackedCard, s.card1)}>
-        <h1 className={s.cardText}></h1>
-      </div>
-      <div className={cn(s.stackedCard, s.card2)}>
-        <h1 className={s.cardText}></h1>
-      </div>
-      <div className={cn(s.stackedCard, s.card3)}>
-        <h1 className={s.cardText}></h1>
-      </div>
-      <div className={cn(s.stackedCard, s.card4)}>
-        <h1 className={s.cardText}>{topText}</h1>
-      </div>
+      {stackTexts.map((item, index) => {
+        if (index == 3) {
+          return (
+            <Draggable
+              axis="x"
+              defaultPosition={{ x: 0, y: 0 }}
+              position={{ x: 0, y: 0 }}
+              scale={1}
+              onStop={() => handleStop(item.index)}
+            >
+              <div className={cn(s.draggableCard, 'layer' + index)}>
+                <div className={cn(s.stackedCard, 'card' + item.index)}>
+                  <h1 className={s.cardText}>{item.text}</h1>
+                </div>
+              </div>
+            </Draggable>
+          )
+        } else {
+          return (
+            <div
+              className={cn(
+                s.stackedCard,
+                'card' + item.index,
+                'layer' + index
+              )}
+            >
+              <h1 className={s.cardText}>{item.text}</h1>
+            </div>
+          )
+        }
+      })}
     </div>
   )
 }
